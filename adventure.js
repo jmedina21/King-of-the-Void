075b24cb6fa42d2846d232ce2a1b483fc884a271
jpmm21 <jmedinamulet@gmail.com>
2023-03-09 15:23:33 +0200

    basic notes added, prologue, character info

---
+++ adventure.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title")
+const windowInfo = document.getElementById("window-info")
+
+document.getElementById("start-btn").addEventListener("click", function() {
+    title.textContent = 'Who are you adventurer?'
+    windowInfo.textContent = "Choose wisely between these 4 mighty heroes"
+    
+})
---
+++ characterInfo.js
@@ -0,0 +1,37 @@
+characterData = [
+    {   race: "orc",
+        stats: {
+            strength: 10,
+            dexterity: 5,
+            intelligence: 3,
+        },
+        inventory: {
+            weapon: {
+                name: "axe",
+                damage: 10,
+            },
+            skills: {
+            },           
+        },
+    },
+    {   race: "human",
+        stats: {
+            strength: 6,
+            dexterity: 6,
+            intelligence: 6,
+        },
+        race:'elf',
+        stats: {
+            strength: 3,
+            dexterity: 8,
+            intelligence: 10,
+        },
+    },
+    {   race: 'dwarf',
+        stats: {
+            strength: 8,
+            dexterity: 3,
+            intelligence: 5,
+        },
+    },
+]
